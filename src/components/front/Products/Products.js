89d5bd12ffb0de4838c5ea144a02e15f7007b518
jacam666 <ja6cam@gmail.com>
2023-05-10 19:28:06 +0100

    more responsive changes

--- src/components/front/Products/Products.js
+++ src/components/front/Products/Products.js
@@ -5,30 +5,35 @@ import data from "../../back/Data/Data";
 const Products = ({ productItems, handleAddProduct }) => {
     return (
         <div className="card-container">
-            {data.productItems && data.productItems.map((productItem) => (
+            {data.productItems &&
+                data.productItems.map((productItem) => (
                     <div className="card" key={productItem.id}>
-                    {/*<Link to={productItem.to}>*/}
                         <div>
-                        <Link to={productItem.to}>
+                            <Link to={productItem.to}>
+                                <div>
+                                    <img
+                                        className="cardImage"
+                                        src={productItem.image}
+                                        alt={productItem.name}
+                                    />
+                                </div>
+                                <div>
+                                    <h2 className="product-name">{productItem.name}</h2>
+                                </div>
+                                <div className="price">£{productItem.price}</div>
+                            </Link>
                             <div>
-                                <img className="cardImage" src={productItem.image} alt={productItem.name} />
+                                <button
+                                    className="product-add-button"
+                                    onClick={() => handleAddProduct(productItem)}
+                                >
+                                    Add to Cart
+                                </button>
                             </div>
-                            <div>
-                                <h2 className="product-name">{productItem.name}</h2>
-                            </div>
-                            <div className="price">£{productItem.price}</div>
-                            </ Link>
-                            <div>
-                                <button className="product-add-button" onClick={() => handleAddProduct(productItem)}>Add to Cart</button>
-                            </div>
-                            
-                            
                         </div>
-                        {/*</Link>*/}
-                    
                     </div>
-            ))}
-        </div>  
+                ))}
+        </div>
     );
 };
 
