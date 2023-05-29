3757b9fecd31df2feaae4c2c7be17eb57a19160e
jacam666 <ja6cam@gmail.com>
2023-05-29 11:06:40 +0100

    changes carousel images and sizing

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import "../components/CarouselImages.css"
-import image1 from "../components/CarouselImages/CarouselfullRange.jpg"
+/*import image1 from "../components/CarouselImages/CarouselfullRange.jpg"
 import image2 from "../components/CarouselImages/CarouselAfterTrain.jpg"
 import image3 from "../components/CarouselImages/CarouselB4Extreme.jpg"
 import image4 from "../components/CarouselImages/CarouselOnlyWheyPromotion.jpg"
-import image5 from "../components/CarouselImages/CarouselStageB4Pump.jpg"
+import image5 from "../components/CarouselImages/CarouselStageB4Pump.jpg"*/
+import image6 from "../components/CarouselImages/FatBurnerBanner.png"
+import image7 from "../components/CarouselImages/OnlyWheYBanner.png"
+import image8 from "../components/CarouselImages/after_train_banner_test_01_2545px.png-1.png"
+import image9 from "../components/CarouselImages/B4ExtremeBanner.png"
 
-const images = [image1, image2, image3, image4, image5];
+const images = [/*image1, image2, image3, image4, image5,*/ image6, image7, image8, image9];
 
 const ImageCarousel = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -14,7 +18,7 @@ const ImageCarousel = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 4000);
+        }, 3500);
 
         return () => clearInterval(interval);
     }, [currentImageIndex]);
@@ -22,7 +26,7 @@ const ImageCarousel = () => {
     const imageCarouselInnerStyle = {
         display: 'flex',
         width: `${images.length * 100}%`,
-        transition: 'transform 1.5s ease-in-out',
+        transition: 'transform 2s ease-in-out',
         transform: `translateX(-${(currentImageIndex / images.length) * 100}%)`,
 
     };
@@ -44,3 +48,4 @@ const ImageCarousel = () => {
 
 };
 export default ImageCarousel;
+
