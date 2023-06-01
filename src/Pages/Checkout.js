0f37a908e8a1b084182eebc49c89edfbb8ec9cf8
jacam666 <ja6cam@gmail.com>
2023-06-01 21:03:59 +0100

    modified the accordion in the checkout

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 import "../components/checkout.css";
 import PayPalCheckout from "../components/PayPalCheckout";
 
@@ -63,84 +63,74 @@ const Checkout = ({ productItem, basketItems = [] }) => {
                             </div>
 
                             <div className="checkout-total">£{totalPrice.toFixed(2)}</div>
+
+
                         </div>
+
                     </div>
                 </div>
-
-                {/*<div className="shipping-details">
-                    <h2 className="address-heading">Billing details:</h2>
-
-                    <input
-                        className="shipping-firstName"
-                        type="text"
-                        placeholder="First Name"
-                        name=""
-                        id="shipping-FirstName"
-                        autoComplete="given-name"
-                    />
-                    <input
-                        className="shipping-lastName"
-                        type="text"
-                        placeholder="Last Name"
-                        name=""
-                        id="shipping-lastName"
-                        autoComplete="family-name"
-                    />
-
-                    <input
-                        className="company-name"
-                        type="text"
-                        placeholder="Company (optional)"
-                        name=""
-                        id="company"
-                        autoComplete="organization"
-                    />
-                    <input
-                        className="address"
-                        type="text"
-                        placeholder="Address"
-                        name=""
-                        id="Address"
-                        autoComplete="address-line1"
-                    />
-                    <input
-                        className="address-line-two"
-                        type="text"
-                        placeholder="Apartment, Suite, etc, (optional)"
-                        name=""
-                        id="address-line2"
-                        autoComplete="address-line2"
-                    />
-                    <input
-                        className="city"
-                        type="text"
-                        placeholder="City"
-                        name=""
-                        id="city"
-                        autoComplete="address-level2"
-                    />
-                    <input
-                        className="postcode"
-                        type="text"
-                        placeholder="Postcode"
-                        name=""
-                        id="postcode"
-                        autoComplete="postal-code"
-                    />
-                    <input
-                        className="phone-number"
-                        type="text"
-                        placeholder="Phone number for order and delivery updates"
-                        name=""
-                        id="phone"
-                        autoComplete="tel"
-                    />
-                                                </div>*/}
-                <PayPalCheckout basketItems={basketItems}  />
+                <PayPalCheckout basketItems={basketItems} />
             </div>
-            
+
         </div>
     );
 };
 
 export default Checkout;
+*/
+import React, { useState } from "react";
+import "../components/checkout.css";
+import PayPalCheckout from "../components/PayPalCheckout";
+
+const Checkout = ({ productItem, basketItems = [] }) => {
+    const [isBasketOpen, setIsBasketOpen] = useState(false);
+    const totalPrice =
+        basketItems.length > 0
+            ? basketItems.reduce(
+                (price, item) => price + item.quantity * item.price,
+                0
+            )
+            : 0;
+
+    const toggleBasket = () => {
+        setIsBasketOpen(!isBasketOpen);
+    };
+
+    const orderSummaryText = isBasketOpen ? "Hide Order Summary" : "Show Order Summary";
+
+    return (
+        <div>
+            <h1 className="checkout-heading">Checkout</h1>
+            <div className="checkout-container">
+                <div className="order-summary">
+                    <h2 className="order-summary-heading" onClick={toggleBasket}>
+                        {orderSummaryText}
+                        <span className="arrow-icon">{isBasketOpen ? "▲" : "▼"}</span>
+                    </h2>
+                    <div className="checkout-basket-dropdown">
+                        {isBasketOpen && (
+                            <div className="accordion-container">
+                                <div className={`accordion-content ${isBasketOpen ? "open" : ""}`}>
+                                    <div className="checkout-basket-items">
+                                        {basketItems.map((item) => (
+                                            <div key={item.id} className="checkout-basket-item">
+                                                <span className="checkout-item-name">{item.name}</span>
+                                                <span className="checkout-item-quantity"> Quantity: {item.quantity}</span>
+                                                <span> Price: {item.quantity * item.price}</span>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    <div className="checkout-total">£{totalPrice.toFixed(2)}</div>
+                </div>
+                <PayPalCheckout basketItems={basketItems} />
+            </div>
+        </div>
+    );
+};
+
+export default Checkout;
+
