c5e2704d16eb945661668afc97231d979545bc8d
jacam666 <ja6cam@gmail.com>
2023-06-15 16:30:03 +0100

    restyles studio page and created studio carousel component

---
+++ src/components/StudioCarousel.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const StudioCarousel = () => {
+  return (
+    <div>StudioCarousel</div>
+  )
+}
+
+export default StudioCarousel
+
+/*
+import React, { useState, useEffect } from 'react';
+import "../components/CarouselImages.css"
+import image1 from "../components/CarouselImages/CarouselfullRange.jpg"
+import image6 from "../components/CarouselImages/FatBurnerBanner.png"
+import image7 from "../components/CarouselImages/OnlyWheYBanner.png"
+import image8 from "../components/CarouselImages/after_train_banner_test_01_2545px.png-1.png"
+import image9 from "../components/CarouselImages/B4ExtremeBanner.png"
+
+const images = [image1, image6, image7, image8, image9];
+
+const ImageCarousel = () => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 4500);
+
+        return () => clearInterval(interval);
+    }, [currentImageIndex]);
+
+    const imageCarouselInnerStyle = {
+        display: 'flex',
+        width: `${images.length * 100}%`,
+        transition: 'transform 2s ease-in-out',
+        transform: `translateX(-${(currentImageIndex / images.length) * 100}%)`,
+
+    };
+
+    return (
+        <div className="image-carousel">
+            <div className="image-carousel-inner" style={imageCarouselInnerStyle}>
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image}
+                        alt=""
+                        className={index === currentImageIndex ? 'visible' : ''}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+
+};
+export default ImageCarousel;*/
+
