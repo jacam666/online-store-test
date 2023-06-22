863f04ee6ec7c91298280e8e7cd62d78a7f73c3e
jacam666 <ja6cam@gmail.com>
2023-06-22 20:17:52 +0100

    added all related products to protein page

--- src/Pages/ProteinCataloguePage.js
+++ src/Pages/ProteinCataloguePage.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import data from '../components/back/Data/Data';
 import "../components/ProteinCatalogue.css";
+import { Link } from 'react-router-dom';
+import AddToCartButton from '../components/AddToCartButton';
 
-const ProteinCataloguePage = () => {
-  return (
-    <div>
-        <h1 className='protein-header'>ProteinCataloguePage</h1>
+const ProteinCataloguePage = ({ handleAddProduct }) => {
+    const specificItemIds = ["1", "5", "2", "3", "4", "7", "8", "16"];
+
+    const filteredProducts = data.productItems.filter((productItem) =>
+        specificItemIds.includes(productItem.id)
+    );
+
+    return (
+        <div>
+            <div className="protein-page-card-container">
+                {filteredProducts.map((productItem) => (
+                    <div className="protein-page-card" key={productItem.id}>
+                        <div>
+                            <Link to={productItem.to} className='protein-page-link'>
+                                <div>
+                                    <img
+                                        className={`protein-page-cardImage protein-page-product-image-${productItem.id}`}
+                                        src={productItem.image}
+                                        alt={productItem.name}
+                                    />
+                                </div>
+                                <div>
+                                    <h2 className="protein-page-product-name">{productItem.name}</h2>
+                                </div>
+                                <div className="protein-page-price">£{productItem.price}</div>
+                            </Link>
+                            <AddToCartButton
+                                handleAddProduct={handleAddProduct}
+                                productItem={productItem}
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-  )
-}
+    );
+};
 
 export default ProteinCataloguePage
