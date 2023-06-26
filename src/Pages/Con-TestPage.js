6a57b61b35a0a97630b5a4ea984be8fb500a56a1
jacam666 <ja6cam@gmail.com>
2023-06-26 10:59:42 +0100

    restyled and added content to the wellbeing products

--- src/Pages/Con-TestPage.js
+++ src/Pages/Con-TestPage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import AddToCartButton from "../components/AddToCartButton";
+import { BsFillHexagonFill } from "react-icons/bs";
 
 const Contest = ({ productItem, handleAddProduct }) => {
     const product = {
         id: "17",
-        image: "./images/PCT-NEW-300x300.png",
+        image: "/images/PCT-NEW-300x300.png",
         name: "CON-TEST P.C.T 240 caps",
         price: 29.99,
         description:
-            "CON-TEST P.C.T HAS BEEN SPECIALLY FORMULATED TO INCREASE NATURAL TESTOSTERONE, REDUCE ESTROGEN AND INHIBIT CORTISOL. MAXIMIZING NATURAL TESTOSTERONE WILL IMPROVE PERFORMANCE, OVERALL WELL BEING AND ELEVATE SEX DRIVE.",
+            "Con-Test P.C.T is a supplement that helps you increase your natural testosterone, reduce your estrogen, and inhibit your cortisol. It contains ingredients such as D-aspartic acid, conjugated linoleic acid, bulbine natalensis, mucuna pruriens, ashwagandha root extract, liquorice root extract, tongkat ali extract, fenugreek seed extract, avena sativa, rhodiola rosea root extract, and bioperine that work together to improve your performance, overall well being, and sex drive. It is suitable for men who want to maximise their muscle gains, strength, and recovery. You can take eight capsules of Con-Test P.C.T daily with food for best results.",
         ingredients: [
             "D-ASPARTIC ACID 1400MG,",
             <br />,
@@ -38,49 +39,82 @@ const Contest = ({ productItem, handleAddProduct }) => {
     return (
         <div className="product-card">
             <div className="product-container">
-            <h2 className="product-name">{product.name}</h2>
                 <div className="product-image-container">
-                <img
-                    className="contest-product-card-image"
-                    src={product.image}
-                    alt={product.name}
-                />
-                <img className="contest-product-card-image-2" src="images\contest-nutri-info-.jpeg" alt="supplements" />
-                </div>
-                
-                <div className="product-card-info">
-                <div className="product-price">
-                    <p>Price: {product.price}</p>
+                    <div className="name-and-bullet-container">
+                        <h2 className="product-name">{product.name}</h2>
+                        <div className="bullet-points-container" >
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={10} />
+                                <p className="product-bullet-points">1400mg of D-Aspartic Acid</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={13} />
+                                <p className="product-bullet-points">700mg of Conjugated Linoleic Acid</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={11} />
+                                <p className="product-bullet-points">300mg of Bulbine Natalensis</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={11} />
+                                <p className="product-bullet-points">250mg of Muccuna Pruriens</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={14} />
+                                <p className="product-bullet-points">200mg of Ashwagandha Root Extract</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={12} />
+                                <p className="product-bullet-points">150mg of Liquorice Root Extract</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={11} />
+                                <p className="product-bullet-points">150mg of Tongkat Ali Extract</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={13} />
+                                <p className="product-bullet-points">100mg of Fenugreek Seed Extract</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={17} />
+                                <p className="product-bullet-points">100mg 0f Avena Sativa (Derived from Oats)</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img
+                        className="product-card-image"
+                        src={product.image}
+                        alt={product.name}
+                    />
                 </div>
 
-                <div className="product-description">
-                    <h1>Description:</h1>
-                    <p>{product.description}</p>
-                </div>
-                <div className="servings-info">
-                    <h1>Servings:</h1>
-                    <p>{product.servings}</p>
-                </div>
-                <div className="product-directions">
-                    <p>{product.directions}</p>
-                </div>
-                <div className="product-advice">
-                    <p>{product.advice}</p>
-                </div>
-                <div className="product-storage">
-                    <p>{product.storage}</p>
-                </div>
-                {/*<div className="product-ingredient">
-                <h1>Ingredients:</h1>
-                <p>{product.ingredients}</p>
-    </div>*/}
+                <div className="product-card-info">
+                    <div className="product-price">
+                        <p>Price: {product.price}</p>
+                    </div>
+
+                    <div className="product-description">
+                        <p>{product.description}</p>
+                    </div>
+                    <div className="servings-info">
+                        <p>{product.servings}</p>
+                    </div>
+                    <div className="product-directions">
+                        <p>{product.directions}</p>
+                    </div>
+                    <div className="product-advice">
+                        <p>{product.advice}</p>
+                    </div>
+                    <div className="product-storage">
+                        <p>{product.storage}</p>
+                    </div>
                 </div>
                 <AddToCartButton
-                handleAddProduct={handleAddProduct}
-                productItem={product}
-            />
+                    handleAddProduct={handleAddProduct}
+                    productItem={product}
+                />
             </div>
-            
+
         </div>
     );
 };
--- src/Pages/YearRoundPage.js
+++ src/Pages/YearRoundPage.js
@@ -1,19 +1,16 @@
 import React from "react";
 import "../ProductCard.css"
 import AddToCartButton from "../components/AddToCartButton";
+import { BsFillHexagonFill } from "react-icons/bs";
 
 const YearRound = ({ productItem, handleAddProduct }) => {
     const product = {
         id: "12",
-        image: "./images/Year-round-300x300.png",
+        image: "/images/Year-round-300x300.png",
         name: "YEAR ROUND IMMUNITY SUPPORT 60 caps",
         price: 19.99,
         description: [
-            "Year Round is a high strength immunity support,",
-            <br />,
-            "this can be taken all year round to help boost immunity",
-            <br />,
-            "and fight off colds and viruses.",
+            "Year Round Immunity Support is a supplement that helps you boost your immune system and fight off colds and viruses. It contains ingredients such as vitamin C, zinc, copper, elderberry, echinacea, garlic, and olive leaf that work together to support your natural defences and protect your health. It is suitable for vegetarians and vegans and does not contain any fillers, bulking agents, or anti-caking agents. You can take one capsule of Year Round Immunity Support daily with food for best results.",
         ],
         servings: "60 caps",
     };
@@ -21,47 +18,76 @@ const YearRound = ({ productItem, handleAddProduct }) => {
     return (
         <div className="product-card">
             <div className="product-container">
-            <h2 className="product-name">{product.name}</h2>
                 <div className="product-image-container">
-                <img
-                    className="year-round-product-card-image"
-                    src={product.image}
-                    alt={product.name}
-                />
-                <img className="year-round-product-card-image-2" src="/images\year-round-nutri-intro.jpeg" alt="vitamins" />
+                    <div className="name-and-bullet-container">
+                        <h2 className="product-name">{product.name}</h2>
+                        <div className="bullet-points-container" >
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={17} />
+                                <p className="product-bullet-points">175mg Echinacea (Echinacea Angustifolia) per capsule</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={20} />
+                                <p className="product-bullet-points">100mg of Astragalus Root (Astragalus membranaceus) per capsule</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={11} />
+                                <p className="product-bullet-points">100mg Olive Tree Extract per capsule</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={19} />
+                                <p className="product-bullet-points">50mg of Beta Glucan 1,3/1,6 (from Saccharomyces Cerevisiae)</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={10} />
+                                <p className="product-bullet-points">50g of Garlic Extract per capsule</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={10} />
+                                <p className="product-bullet-points">120mg of Vitamin C (Ascorbic Acid)</p>
+                            </div>
+                            <div className="bullet-points">
+                                <BsFillHexagonFill className="hexagon-icon" size={11} />
+                                <p className="product-bullet-points">10mg of Vitamin E (D-Alpha Tocophery)</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img
+                        className="product-card-image"
+                        src={product.image}
+                        alt={product.name}
+                    />
                 </div>
-                
+
                 <div className="product-card-info">
-                <div className="product-price">
-                    <p>Price: {product.price}</p>
-                </div>
+                    <div className="product-price">
+                        <p>Price: {product.price}</p>
+                    </div>
 
-                <div className="product-description">
-                    <h1>Description:</h1>
-                    <p>{product.description}</p>
-                </div>
-                <div className="servings-info">
-                    <h1>Servings:</h1>
-                    <p>{product.servings}</p>
-                </div>
-                <div className="product-directions">
-                    <p>{product.directions}</p>
-                </div>
-                <div className="product-advice">
-                    <p>{product.advice}</p>
-                </div>
-                <div className="product-storage">
-                    <p>{product.storage}</p>
-                </div>
+                    <div className="product-description">
+                        <p>{product.description}</p>
+                    </div>
+                    <div className="servings-info">
+                        <p>{product.servings}</p>
+                    </div>
+                    <div className="product-directions">
+                        <p>{product.directions}</p>
+                    </div>
+                    <div className="product-advice">
+                        <p>{product.advice}</p>
+                    </div>
+                    <div className="product-storage">
+                        <p>{product.storage}</p>
+                    </div>
 
-                <p className="product-ingredient">{product.ingredients}</p>
+                    <p className="product-ingredient">{product.ingredients}</p>
                 </div>
                 <AddToCartButton
-                handleAddProduct={handleAddProduct}
-                productItem={product}
-            />
+                    handleAddProduct={handleAddProduct}
+                    productItem={product}
+                />
             </div>
-            
+
         </div>
     );
 }
