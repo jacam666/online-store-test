e4bbb764b9ef3b8f6b278156d1be4b50c5370056
jacam666 <ja6cam@gmail.com>
2023-06-27 19:35:19 +0100

    fatloss and training/diet pages are now responsive

--- src/Pages/TrainingAndDietPage.js
+++ src/Pages/TrainingAndDietPage.js
@@ -20,7 +20,7 @@ const TrainingAndDietPage = ({ productItem, handleAddProduct }) => {
                     <div className="training-page-card" key={productItem.id}>
                         <div>
                             <Link to={productItem.to} className='training-page-link'>
-                                <div>
+                                <div className='training-page-image-container'>
                                     <img
                                         className={`training-page-cardImage training-page-product-image-${productItem.id}`}
                                         src={productItem.image}
@@ -28,7 +28,7 @@ const TrainingAndDietPage = ({ productItem, handleAddProduct }) => {
                                     />
                                 </div>
 
-                                <div>
+                                <div className='training-page-name-container'>
                                     <h2 className={`training-page-product-name training-page-product-name-${productItem.id}`}>
                                         {productItem.name}
                                     </h2>
@@ -47,9 +47,11 @@ const TrainingAndDietPage = ({ productItem, handleAddProduct }) => {
                     </div>
                 ))}
                 <div className='training-image-container'>
+                    <div className='certificate-and-text-container'>
                     <img className='training-certificate' src='/images/nutritionCertificate.jpg' alt='certificate' />
                     <div className='training-studio-text-container'>
                         <p className='training-studio-text'>If you'd like to train face to face on a 1 to 1 basis or in small groups take a look at our state of the art SNC studio.</p>
+                    </div>
                     </div>
                     <img className='training-studio' src={studioImage} alt='studio' />
                     <Link to="/TheStudioPage">
