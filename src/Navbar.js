71daf246ca9b8c71d294ab4f38ff53b7103c755e
jacam666 <ja6cam@gmail.com>
2023-08-13 10:57:39 +0100

    added handlenavclick function to all navbar links to close the navbar after being clicked

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,23 +5,32 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import "./components/Navbar.css"
 import BasketIcon from './BasketIcon';
+import { useState } from 'react';
 
 function MyNavbar() {
+
+  const [expanded, setExpanded] = useState(false);
+
+  const handelNavClick = () => {
+    setExpanded(false);
+  }
+
+
     return (
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body-tertiary" expanded={expanded}>
       <Container>
-        <Navbar.Brand as={Link} to='/'>UKSNC Supplements</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand as={Link} to='/' onClick={handelNavClick}>UKSNC Supplements</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto d-flex justify-content-center align-items-center">
-                        <Nav.Link as={Link} to='/' className="text-dark mr-5">Home</Nav.Link>
-                        <Nav.Link as={Link} to='/ProteinCataloguePage' className="text-dark mr-5">Proteins</Nav.Link>
-                        <Nav.Link as={ Link } to='/PreWorkoutCataloguePage' className="text-dark mr-5">Pre-Workouts</Nav.Link>
-                        <Nav.Link as={ Link } to='/FatLossCataloguePage' className="text-dark mr-5">Fat Loss</Nav.Link>
-                        <Nav.Link as={ Link } to='/WellBeingCataloguePage' className="text-dark mr-5">General Wellbeing</Nav.Link>
-                        <Nav.Link as={ Link } to='/TrainingAndDietPage' className="text-dark mr-5">Training and Diet Plans</Nav.Link>
-                        <Nav.Link as={ Link } to='/TheStudioPage' className="text-dark mr-5">The Studio</Nav.Link>
-                        <Nav.Link as={ Link } to='/BasketPage' className="text-dark mr-5 "><BasketIcon /></Nav.Link>
+                        <Nav.Link as={Link} to='/' className="text-dark mr-5" onClick={handelNavClick}>Home</Nav.Link>
+                        <Nav.Link as={Link} to='/ProteinCataloguePage' className="text-dark mr-5" onClick={handelNavClick}>Proteins</Nav.Link>
+                        <Nav.Link as={ Link } to='/PreWorkoutCataloguePage' className="text-dark mr-5" onClick={handelNavClick}>Pre-Workouts</Nav.Link>
+                        <Nav.Link as={ Link } to='/FatLossCataloguePage' className="text-dark mr-5" onClick={handelNavClick}>Fat Loss</Nav.Link>
+                        <Nav.Link as={ Link } to='/WellBeingCataloguePage' className="text-dark mr-5" onClick={handelNavClick}>General Wellbeing</Nav.Link>
+                        <Nav.Link as={ Link } to='/TrainingAndDietPage' className="text-dark mr-5" onClick={handelNavClick}>Training and Diet Plans</Nav.Link>
+                        <Nav.Link as={ Link } to='/TheStudioPage' className="text-dark mr-5" onClick={handelNavClick}>The Studio</Nav.Link>
+                        <Nav.Link as={ Link } to='/BasketPage' className="text-dark mr-5" onClick={handelNavClick}><BasketIcon /></Nav.Link>
                     </Nav>
         </Navbar.Collapse>
       </Container>
