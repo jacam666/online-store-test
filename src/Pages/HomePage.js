f4cdc37980d1580ddbcddcf8ad0881d69b360071
jacam666 <ja6cam@gmail.com>
2023-08-13 10:19:07 +0100

    converted the styling of the studio page to bootstrap

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -11,34 +11,27 @@ import { Link } from 'react-router-dom';
 function HomePage() {
   return (
     <>
-
       <div className="row">
         <div className="col-12 col-md-6">
           <Card>
             <Link to='/ProteinCataloguePage'>
             <Card.Img variant="bottom" src={HomePageBanner} />
             </Link>
-            
             <Card.Body>
               <Card.Text className='homepage-text'>
-                <p className='mb-0'>
                   <span className='homepage-heading'>SNC Supplements</span> are a range of products designed to help you achieve your fitness goals, whether you want to build muscle,lose fat, improve performance or boost your immune system.
-                </p>
               </Card.Text>
             </Card.Body>
           </Card>
         </div>
-        
         <div className='col-12 col-md-6'>
           <Card>
             <Link to='/ProteinCataloguePage'>
             <Card.Img variant="bottom" src={AfterTrainImage} />
             </Link>
             <Card.Body>
-              <Card.Text className='homepage-text'>
-                <p className='mb-0'>
+              <Card.Text className='homepage-text mb-0'>
                   <span className='homepage-heading'>After Train: </span>A post-workout formula that contains, whey protein isolate, creatine monohydrate, glutamine and other ingredients to replenish your muscles, restore your glycogen and support your recovery.
-                </p>
               </Card.Text>
             </Card.Body>
           </Card>
@@ -54,9 +47,7 @@ function HomePage() {
             </Link>
             <Card.Body>
               <Card.Text className='homepage-text'>
-                <p className='mb-0'>
                   <span className='homepage-heading'>B4 Train:</span> A pre-workout formula that contains caffeine, beta-alanine, citrulline malate and other ingredients to enhance your energy, focus, pump and endurance.
-                </p>
               </Card.Text>
             </Card.Body>
           </Card>
@@ -69,9 +60,7 @@ function HomePage() {
             </Link>
             <Card.Body>
               <Card.Text className='homepage-text'>
-                <p className='mb-0'>
                 <span className='homepage-heading'>Are</span> you looking for somewhere you can achieve your fitness goals? Whether you want to lose weight, build muscle or improve your endurance then check out SNC Studio.
-                </p>
               </Card.Text>
             </Card.Body>
           </Card>
--- src/Pages/TheStudioPage.js
+++ src/Pages/TheStudioPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import "../TheStudio.css"
 import StudioCarousel from '../components/StudioCarousel'
 import image9 from "../components/StudioImages/studioPic9.jpg"
 import imageText1 from "../components/StudioImages/13759 copy studioimage1.png"
@@ -7,54 +6,41 @@ import imageText2 from "../components/StudioImages/13759 copy studioimage2.png"
 import imageText3 from "../components/StudioImages/13759 copy studioimage3.png"
 import imageText4 from "../components/StudioImages/13759 copy studioimage4.png"
 import imageText5 from "../components/StudioImages/13759 copy studioimage5.png"
+import Card from 'react-bootstrap/Card'
 
 function TheStudioPage() {
     return (
         <div className='studio-container'>
-            
-            <div className='studio-image-and-text-container'>
                 <StudioCarousel />
                 <div>
-                <h1 className='studio-heading'>SNC Studio</h1>
-            </div>
-                <div className='studio-descriptions'>
-                    <img className='studio-image-text-1' src={imageText1} alt='studioText' />
-                    <img className='studio-image-text-1' src={imageText2} alt='studioText' />
-                    <img className='studio-image-text-1' src={imageText3} alt='studioText' />
-                    <img className='studio-image-text-1' src={imageText4} alt='studioText' />
-                    <img className='studio-image-text-1' src={imageText5} alt='studioText' />
-                    {/*} <p className='studio-description-section-1'>
-                            Are you looking for a personal trainer that can help you achieve your goals in a comfortable and private environment?
-                        </p>
-                        <div className='studio-description-section-2'>
-                            Do you want to train with a bodybuilder and competitor who has won 
-                            multiple competitions, is a level 4 nutritional coach and has 
-                            30 years of experience in the industry?
-                        </div>
-                        <div className='studio-description-section-3'>
-                            If so, then you have come to the right place. 
-                            Welcome to Snc Studio, where we offer 
-                            one-to-one and group sessions with Mark Etherden, 
-                            a certified personal trainer and a successful bodybuilder.
-                        </div>
-                        <div className='studio-description-section-4'>
-                            Our Studio is equipped with air conditioning and 
-                            state-of-the-art equipment. Whether you want to lose weight, 
-                            gain muscle, improve your health, or prepare for a 
-                            competition, we have the program for you.
-                        </div>
-                        <p className='studio-description-section-5'>
-                            Contact us today and book your free consultation with Mark Etherden. You won’t regret it!
-                        </p>
-    */}
+                    <h1 className='studio-heading'>SNC Studio</h1>
                 </div>
-                <img className='studio-image-9' src={image9} alt='personal-training' />
-            </div>
-
-
+                <div className='row'>
+                    <div className='col-12 col-md-6'>
+                            <Card.Img src={imageText1} />
+                    </div>
+                    <div className='col-12 col-md-6'>
+                        <Card.Img src={imageText2} />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-12 col-md-6'>
+                        <Card.Img src={imageText3} />
+                    </div>
+                    <div className='col-12 col-md-6'>
+                        <Card.Img src={imageText4} />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-12 col-md-6'>
+                        <Card.Img src={imageText5} className='studio-image-5' />
+                    </div>
+                    <div className='col-12 col-md-6'>
+                    <img className='studio-image-9' src={image9} alt='personal-training' />
+                    </div>
+                </div>
+                
         </div>
-
-
     )
 }
 
