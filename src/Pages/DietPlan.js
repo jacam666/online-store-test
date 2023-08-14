05b86a2442e2a25fd24558ad560bfcf63c8145dc
jacam666 <ja6cam@gmail.com>
2023-08-14 14:11:19 +0100

    converted diet-plan-page to bootstrap styling

--- src/Pages/DietPlan.js
+++ src/Pages/DietPlan.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import "../components/DietPlan.css"
 import AddToCartButton from '../components/AddToCartButton'
-//import dietText1 from "/images/brush_strokes text1.jpg"
+import Card from 'react-bootstrap/Card'
+import { Col, Row } from 'react-bootstrap'
 
 
 const DietPlan = ({ productItem, handleAddProduct }) => {
@@ -20,6 +21,26 @@ const DietPlan = ({ productItem, handleAddProduct }) => {
                     <div className='header'>Diet Plan</div>
                     <p className='header-2'>With 1 hour online consultation</p>
                 </div>
+                <div className='row d-flex'>
+                    <div className='col-12 col-md-8 mx-auto'>
+                        <Card className=''>
+                            <Card.Body>
+                                <Row>
+                                    <Col>
+                                        <Card.Img className='diet-plan-image' src='/images/personal-training-image-1.jpg' />
+                                    </Col>
+                                    <Col>
+                                        <Card.Img className='diet-plan-image' src='/images/nutritionCertificate.jpg' />
+                                    </Col>
+                                </Row>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+                <div className="diet-plan-product-price">
+                    <p>Price: £{product.price}</p>
+                </div>
+                {/*}
                 <div className='diet-plan-image-container'>
                     <img className='diet-image' src='/images/personal-training-image-1.jpg' alt='personal-training' />
                     <img className='certificate' src='/images/nutritionCertificate.jpg' alt='nutrition-certificate' />
@@ -27,34 +48,36 @@ const DietPlan = ({ productItem, handleAddProduct }) => {
                 <div className="diet-plan-product-price">
                     <p>Price: £{product.price}</p>
                 </div>
-                {/*<div className='diet-plan-text-container'>*/}
-                    {/*<img className='diet-text-1' src="/images/brush_strokes text1.jpg" alt='diet-text' />
-                    <img className='diet-text-1' src="/images/brush_strokes text2.jpg" alt='diet-text' />
-                    <img className='diet-text-1' src="/images/brush_strokes text1.jpg" alt='diet-text' />
-                    <img className='diet-text-1' src="/images/brush_strokes text1.jpg" alt='diet-text' />
-    <img className='diet-text-1' src="/images/brush_strokes text1.jpg" alt='diet-text' />*/}
-                    {/*<img className='diet-text-1' src='/images/grunge-border_1048-7373 bulktext.jpg' alt='text' />*/}
-                    <p className='diet-plan-text-1'>
-                        <span className='text-capital'>D</span>o you dream of transforming your body and feeling amazing?
-                    </p>
-                    <p className='diet-plan-text-2'>
-                    <span className='text-capital'>I</span>f so, you need Mark Etherden, the level 4 nutritional coach who has helped hundreds of people achieve their goals.
-                    </p>
-                    <p className='diet-plan-text-3'>
-                    <span className='text-capital'>M</span>ark has 30 years of experience in creating personalized diet plans that suit your lifestyle, preferences and needs.
-                    </p>
-                    <p className='diet-plan-text-4'>
-                    <span className='text-capital'>H</span>e will also guide you through an online session where he will share his tips and tricks, and answer any questions you have. And the best part is, he won’t leave you hanging after that. He will be there to support you, adjust your plan and celebrate your progress.
-                    </p>
-                    <p className='diet-plan-text-5'>
-                        <span className='text-capital'>D</span>on’t miss this opportunity to work with Mark and unleash your potential!
-                    </p>
-    
-                {/*</div>*/}
+    */}
+                <div className='row'>
+                    <div className='col-10 col-md-8 mx-auto'>
+                        <Card className=''>
+                            <Card.Body className=' justify-content-center align-items-center'>
+                                <Card.Text>
+                                    Do you dream of transforming your body and feeling amazing?
+                                </Card.Text>
+                                <Card.Text>
+                                    If so, you need Mark Etherden, the level 4 nutritional coach who has helped hundreds of people achieve their goals.
+                                </Card.Text>
+                                <Card.Text>
+                                    Mark has 30 years of experience in creating personalized diet plans that suit your lifestyle, preferences and needs.
+                                </Card.Text>
+                                <Card.Text>
+                                    He will also guide you through an online session where he will share his tips and tricks, and answer any questions you have. And the best part is, he won’t leave you hanging after that. He will be there to support you, adjust your plan and celebrate your progress.
+                                </Card.Text>
+                                <Card.Text>
+                                    Don’t miss this opportunity to work with Mark and unleash your potential!
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+
+
                 <AddToCartButton
-                        handleAddProduct={handleAddProduct}
-                        productItem={product}
-                    />
+                    handleAddProduct={handleAddProduct}
+                    productItem={product}
+                />
             </div>
         </div>
 
